2115ae36620142c65543864191251fbd26d4c163
Salo Conde <salomecj@gmail.com>
2024-04-11 19:42:40 +0200

    iteration1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
+
+console.log("!Estoy listo¡")
+
 // Iteration 1: Names and Input
-//
+
+let hacker1 = "Peter";
+console.log("El nombre del conductor es: ", hacker1);
+
+let hacker2 = "Jack";
+console.log("El nombre del navegante es:", hacker2);
+
+
+
 // Iteration 2: Conditionals
+
+
+
+
+
 
 
 // Iteration 3: Loops
